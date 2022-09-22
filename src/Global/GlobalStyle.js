0426f3205793d8f7d773a309617291b27ee9f5ef
cosme7 <cosmewr@gmail.com>
2022-09-21 22:25:23 -0300

    First Commit

---
+++ src/Global/GlobalStyle.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    
+    *{
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+
+    :root{
+        /* Font Color */
+        --clr-one: #373737;
+        /* Font-Family */
+        --ff-primary: 'Montserrat', sans-serif; /* Montserrat => Hurme Geometric */
+        --ff-footer: 'Roboto', sans-serif;
+        /* Font Weight */
+        --fw-600: 600;
+        --fw-400: 400;
+        /* Background Color */
+        --bg-one: #FFFFFF;
+        --bg-two: #F2F4F1;
+        --bg-three: #DFE4DE;
+        --bg-four: #446061;
+        --bg-five: #373737;
+    }
+`;
